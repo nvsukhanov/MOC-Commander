fe2570cd5e8f8d104a23d6a3e919f4a6cad32201
Nikolai Sukhanov <25166404+nvsukhanov@users.noreply.github.com>
2023-06-01 01:51:04 +0400

    minor design fixes (#108)
    + bugfix creation of binding w/ gamepad & multiple hubs

--- src/app/control-schemes/edit/control-scheme-edit.component.ts
+++ src/app/control-schemes/edit/control-scheme-edit.component.ts
@@ -1,15 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CONTROL_SCHEME_ACTIONS, CONTROL_SCHEME_SELECTORS, ControlScheme, ROUTER_SELECTORS } from '../../store';
-import { map, Observable, of, Subscription, switchMap } from 'rxjs';
+import { map, Observable, of, Subscription, switchMap, take } from 'rxjs';
 import { BindingFormResult, ControlSchemeEditFormComponent } from './edit-form';
-import { TranslocoModule } from '@ngneat/transloco';
-import { JsonPipe, NgIf } from '@angular/common';
 import { PushPipe } from '@ngrx/component';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
-import { FeatureToolbarService } from '../../common';
 import { RoutesBuilderService } from '../../routing';
+import { TranslocoModule } from '@ngneat/transloco';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
     standalone: true,
@@ -18,22 +16,15 @@ import { RoutesBuilderService } from '../../routing';
     styleUrls: [ './control-scheme-edit.component.scss' ],
     imports: [
         ControlSchemeEditFormComponent,
+        PushPipe,
         TranslocoModule,
         NgIf,
-        PushPipe,
-        JsonPipe,
-        MatButtonModule,
-        RouterLink,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ControlSchemeEditComponent implements OnDestroy {
     public readonly currentlyEditedScheme$: Observable<ControlScheme | undefined> = this.store.select(ROUTER_SELECTORS.selectCurrentlyEditedSchemeId).pipe(
         switchMap((i) => i === null ? of(undefined) : this.store.select(CONTROL_SCHEME_SELECTORS.selectScheme(i)))
-    );
-
-    public readonly isSchemeRunning$: Observable<boolean> = this.store.select(ROUTER_SELECTORS.selectCurrentlyEditedSchemeId).pipe(
-        switchMap((i) => i === null ? of(false) : this.store.select(CONTROL_SCHEME_SELECTORS.isSchemeRunning(i)))
     );
 
     public readonly cancelViewUrl$: Observable<string[]> = this.store.select(ROUTER_SELECTORS.selectCurrentlyEditedSchemeId).pipe(
@@ -44,32 +35,24 @@ export class ControlSchemeEditComponent implements OnDestroy {
 
     constructor(
         private readonly store: Store,
-        private readonly featureToolbarService: FeatureToolbarService,
-        private readonly routesBuilderService: RoutesBuilderService
+        private readonly routesBuilderService: RoutesBuilderService,
+        private readonly router: Router
     ) {
-    }
-
-    @ViewChild('controlsTemplate', { static: false, read: TemplateRef })
-    public set controlsTemplate(controls: TemplateRef<unknown> | null) {
-        this.sub?.unsubscribe();
-        if (!controls) {
-            return;
-        }
-        this.sub = this.currentlyEditedScheme$.subscribe((scheme) => {
-            if (scheme) {
-                this.featureToolbarService.setControls(controls);
-            } else {
-                this.featureToolbarService.clearConfig();
-            }
-        });
     }
 
     public ngOnDestroy(): void {
         this.sub?.unsubscribe();
-        this.featureToolbarService.clearConfig();
     }
 
     public onSave(data: BindingFormResult): void {
         this.store.dispatch(CONTROL_SCHEME_ACTIONS.update(data));
+    }
+
+    public onCancel(): void {
+        this.currentlyEditedScheme$.pipe(
+            take(1)
+        ).subscribe((i) => {
+            this.router.navigate(i === undefined ? this.routesBuilderService.controlSchemesList : this.routesBuilderService.controlSchemeView(i.id));
+        });
     }
 }
