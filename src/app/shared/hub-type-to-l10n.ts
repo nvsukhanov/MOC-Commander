c9802f80ccf99958ce64ff29cc7055e7d3c8c0bc
Nikolai Sukhanov <nvsukhanov@outlook.com>
2023-06-06 15:09:03 +0400

    reorganise i18n assets

--- src/app/shared/hub-type-to-l10n.ts
+++ src/app/shared/hub-type-to-l10n.ts
@@ -1,10 +1,10 @@
 import { HubType } from '@nvsukhanov/rxpoweredup';
 
 export const HUB_TYPE_TO_L10N_MAPPING: Readonly<{ [type in HubType]: string }> = {
-    [HubType.BoostHub]: 'hubTypeBoost',
-    [HubType.WeDoHub]: 'hubTypeWeDo',
-    [HubType.DuploTrain]: 'hubTypeDuploTrain',
-    [HubType.TwoPortHub]: 'hubTypeTwoPortHub',
-    [HubType.TwoPortHandset]: 'hubTypeTwoPortHandset',
-    [HubType.Unknown]: 'hubTypeUnknown'
+    [HubType.BoostHub]: 'hubTypes.boost',
+    [HubType.WeDoHub]: 'hubTypes.weDo',
+    [HubType.DuploTrain]: 'hubTypes.duploTrain',
+    [HubType.TwoPortHub]: 'hubTypes.twoPortHub',
+    [HubType.TwoPortHandset]: 'hubTypes.twoPortHandset',
+    [HubType.Unknown]: 'hubTypes.unknown'
 };
