9391e91d3a4e499c66128b2d022024a0b818fbcd
Nikolai Sukhanov <nvsukhanov@outlook.com>
2023-09-10 16:02:37 +0400

    fix: prevent not using acc/dec profiles to stop scheme from starting

--- src/app/store/effects/tasks-processing/pre-run-scheme.effect.ts
+++ src/app/store/effects/tasks-processing/pre-run-scheme.effect.ts
@@ -1,5 +1,5 @@
 import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
-import { Observable, filter, forkJoin, map, switchMap } from 'rxjs';
+import { Observable, filter, forkJoin, map, of, switchMap } from 'rxjs';
 import { inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CONTROL_SCHEME_ACTIONS, CONTROL_SCHEME_SELECTORS, ControlSchemeModel, HubStorageService, attachedIosIdFn } from '@app/store';
@@ -7,38 +7,36 @@ import { CONTROL_SCHEME_ACTIONS, CONTROL_SCHEME_SELECTORS, ControlSchemeModel, H
 function createSetAccelerationProfileTasks(
     scheme: ControlSchemeModel,
     hubStorage: HubStorageService
-): Observable<unknown> {
+): Array<Observable<unknown>> {
     const ioIdsWithAccelerationProfiles = new Set(
         scheme.bindings.filter((b) => b.useAccelerationProfile).map((b) => attachedIosIdFn(b))
     );
-    const hubTasks = scheme.portConfigs.filter((portConfig) => ioIdsWithAccelerationProfiles.has(attachedIosIdFn(portConfig)))
-                           .map((portConfig) => {
-                               return hubStorage
-                                   .get(portConfig.hubId)
-                                   .motors
-                                   .setAccelerationTime(portConfig.portId, portConfig.accelerationTimeMs);
-                           });
-    return forkJoin(hubTasks);
+    return scheme.portConfigs.filter((portConfig) => ioIdsWithAccelerationProfiles.has(attachedIosIdFn(portConfig)))
+                 .map((portConfig) => {
+                     return hubStorage
+                         .get(portConfig.hubId)
+                         .motors
+                         .setAccelerationTime(portConfig.portId, portConfig.accelerationTimeMs);
+                 });
 }
 
 function createSetDecelerationProfileTasks(
     scheme: ControlSchemeModel,
     hubStorage: HubStorageService
-): Observable<unknown> {
+): Array<Observable<unknown>> {
     const ioIdsWithDecelerationProfiles = new Set(
         scheme.bindings.filter((b) => b.useDecelerationProfile).map((b) => attachedIosIdFn(b))
     );
-    const hubTasks = scheme.portConfigs.filter((portConfig) => ioIdsWithDecelerationProfiles.has(attachedIosIdFn(portConfig)))
-                           .map((portConfig) => {
-                               return hubStorage
-                                   .get(portConfig.hubId)
-                                   .motors
-                                   .setDecelerationTime(portConfig.portId, portConfig.decelerationTimeMs);
-                           });
-    return forkJoin(hubTasks);
+    return scheme.portConfigs.filter((portConfig) => ioIdsWithDecelerationProfiles.has(attachedIosIdFn(portConfig)))
+                 .map((portConfig) => {
+                     return hubStorage
+                         .get(portConfig.hubId)
+                         .motors
+                         .setDecelerationTime(portConfig.portId, portConfig.decelerationTimeMs);
+                 });
 }
 
-export const PREPARE_SCHEME_EFFECT = createEffect((
+export const PRE_RUN_SCHEME_EFFECT = createEffect((
     actions: Actions = inject(Actions),
     hubStorage: HubStorageService = inject(HubStorageService),
     store: Store = inject(Store)
@@ -49,13 +47,14 @@ export const PREPARE_SCHEME_EFFECT = createEffect((
         map(([ , scheme ]) => scheme),
         filter((scheme): scheme is ControlSchemeModel => !!scheme),
         switchMap((scheme) => {
-            const setAccProfileTasks = createSetAccelerationProfileTasks(scheme, hubStorage);
-            const setDecProfileTasks = createSetDecelerationProfileTasks(scheme, hubStorage);
-
-            return forkJoin([
-                setAccProfileTasks,
-                setDecProfileTasks
-            ]).pipe(
+            const combinedTasks = [
+                ...createSetAccelerationProfileTasks(scheme, hubStorage),
+                ...createSetDecelerationProfileTasks(scheme, hubStorage)
+            ];
+            if (combinedTasks.length === 0) {
+                return of({ name: scheme.name });
+            }
+            return forkJoin(combinedTasks).pipe(
                 map(() => ({ name: scheme.name }))
             );
         }),
