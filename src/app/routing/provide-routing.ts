5d3d6d976b8b52520acb518546d6e2cfa6a97e5c
Nikolai Sukhanov <nvsukhanov@outlook.com>
2023-09-11 01:02:01 +0400

    feat: enforce lazy modules preloading

--- src/app/routing/provide-routing.ts
+++ src/app/routing/provide-routing.ts
@@ -1,8 +1,8 @@
-import { provideRouter, withHashLocation } from '@angular/router';
+import { PreloadAllModules, provideRouter, withHashLocation, withPreloading } from '@angular/router';
 import { EnvironmentProviders } from '@angular/core';
 
 import { ROUTES } from './routes';
 
 export function provideRouting(): EnvironmentProviders {
-    return provideRouter(ROUTES, withHashLocation());
+    return provideRouter(ROUTES, withHashLocation(), withPreloading(PreloadAllModules));
 }
