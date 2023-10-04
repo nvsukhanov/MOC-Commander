b9e13813867c15f2d10bb6a2604f9a3440a81659
Nikolai Sukhanov <nvsukhanov@outlook.com>
2023-10-04 19:46:27 +0400

    fix: remove "discover button" ripple due to leaking effect

--- src/app/root/nav-menu/discover-hub-button/discover-hub-button.component.ts
+++ src/app/root/nav-menu/discover-hub-button/discover-hub-button.component.ts
@@ -1,12 +1,10 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { TranslocoPipe } from '@ngneat/transloco';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { PushPipe } from '@ngrx/component';
-import { Subscription, interval, take } from 'rxjs';
-import { MatRipple, MatRippleModule } from '@angular/material/core';
 import { RoutesBuilderService } from '@app/routing';
 
 @Component({
@@ -21,11 +19,10 @@ import { RoutesBuilderService } from '@app/routing';
         MatIconModule,
         RouterLink,
         PushPipe,
-        MatRippleModule
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DiscoverHubButtonComponent implements OnInit, OnDestroy {
+export class DiscoverHubButtonComponent {
     @Input() public isBluetoothAvailable = false;
 
     @Input() public isDiscoveryBusy = false;
@@ -34,14 +31,6 @@ export class DiscoverHubButtonComponent implements OnInit, OnDestroy {
 
     @Output() public readonly discoveryStart = new EventEmitter<void>();
 
-    @ViewChild('discoverHubRipples', { static: false, read: MatRipple }) public readonly discoverHubRipples?: MatRipple;
-
-    private readonly ripplesInterval = 2000;
-
-    private readonly ripplesCount = 3;
-
-    private rippleSub?: Subscription;
-
     constructor(
         public readonly routesBuilderService: RoutesBuilderService,
     ) {
@@ -49,23 +38,5 @@ export class DiscoverHubButtonComponent implements OnInit, OnDestroy {
 
     public onDiscoveryStart(): void {
         this.discoveryStart.next();
-    }
-
-    public ngOnInit(): void {
-        const shouldShowRipple = this.isBluetoothAvailable && !this.isDiscoveryBusy && this.connectedHubsCount === 0;
-        if (shouldShowRipple) {
-            this.rippleSub = interval(this.ripplesInterval).pipe(
-                take(this.ripplesCount)
-            ).subscribe(() => {
-                this.discoverHubRipples?.launch({
-                    persistent: false,
-                    centered: true,
-                });
-            });
-        }
-    }
-
-    public ngOnDestroy(): void {
-        this.rippleSub?.unsubscribe();
     }
 }
