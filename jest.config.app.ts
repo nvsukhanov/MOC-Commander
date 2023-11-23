39bff6056c216a321731bf90f8028ae43adb066f
Nikolai Sukhanov <nvsukhanov@outlook.com>
2023-11-24 01:35:28 +0400

    refactor: make modules/shared not buildable

--- jest.config.app.ts
+++ jest.config.app.ts
@@ -1,9 +1,12 @@
-/* eslint-disable */
-export default {
-    displayName: 'shared',
-    preset: '../../jest.preset.js',
+import type { Config } from '@jest/types';
+
+const esModules = ['@angular', 'tslib', 'rxjs', 'rxpoweredup'];
+
+const CONFIG: Config.InitialOptions = {
+    displayName: 'app',
+    preset: './jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-    coverageDirectory: '../../coverage/modules/shared',
+    coverageDirectory: '../../coverage/modules/app',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': [
             'jest-preset-angular',
@@ -15,9 +18,18 @@ export default {
         ],
     },
     maxWorkers: 1,
+    transformIgnorePatterns: [
+        `node_modules/(?!.*\\.mjs$|${esModules.join('|')})`
+    ],
     snapshotSerializers: [
         'jest-preset-angular/build/serializers/no-ng-attributes',
         'jest-preset-angular/build/serializers/ng-snapshot',
         'jest-preset-angular/build/serializers/html-comment',
     ],
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+        '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+    ]
 };
+
+export default CONFIG;
--- jest.config.ts
+++ jest.config.ts
@@ -1,32 +1,5 @@
-import type { Config } from '@jest/types';
-import nxPreset from '@nx/jest/preset';
+import { getJestProjects } from '@nx/jest';
 
-const esModules = ['@angular', 'tslib', 'rxjs', 'rxpoweredup'];
-
-const CONFIG: Config.InitialOptions = {
-    displayName: 'app',
-    ...nxPreset,
-    setupFilesAfterEnv: ['./src/test-setup.ts'],
-    coverageDirectory: '../../coverage/modules/app',
-    transform: {
-        '^.+\\.(ts|mjs|js|html)$': [
-            'jest-preset-angular',
-            {
-                tsconfig: './tsconfig.spec.json',
-                stringifyContentPathRegex: '\\.(html|svg)$',
-                useESM: true
-            },
-        ],
-    },
-    maxWorkers: 1,
-    transformIgnorePatterns: [
-        `node_modules/(?!.*\\.mjs$|${esModules.join('|')})`
-    ],
-    snapshotSerializers: [
-        'jest-preset-angular/build/serializers/no-ng-attributes',
-        'jest-preset-angular/build/serializers/ng-snapshot',
-        'jest-preset-angular/build/serializers/html-comment',
-    ],
+export default {
+    projects: getJestProjects(),
 };
-
-export default CONFIG;
