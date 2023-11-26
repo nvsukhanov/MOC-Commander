b59bcf17bc23d0ac62fb75b3025bfa2cd8917e1e
Nikolai Sukhanov <nvsukhanov@outlook.com>
2023-11-26 19:19:09 +0400

    fix: add hub connectivity check for CS start blockers of widgets

--- modules/features/control-scheme-view/src/lib/control-scheme-start-blocker-widgets.service.ts
+++ modules/features/control-scheme-view/src/lib/control-scheme-start-blocker-widgets.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { PortModeName } from 'rxpoweredup';
 import { Dictionary } from '@ngrx/entity';
 import { WidgetType } from '@app/shared-misc';
-import { AttachedIoModel, WidgetConfigModel, attachedIosIdFn } from '@app/store';
+import { AttachedIoModel, TemperatureWidgetConfigModel, TiltWidgetConfigModel, VoltageWidgetConfigModel, WidgetConfigModel, attachedIosIdFn } from '@app/store';
 
 import { TemperatureWidgetConfigFactoryService, TiltWidgetConfigFactoryService, VoltageWidgetConfigFactoryService } from './widgets';
 import { SchemeRunBlocker } from './types';
@@ -19,30 +19,72 @@ export class ControlSchemeStartBlockerWidgetsService {
     public getBlockers(
         widgetConfig: WidgetConfigModel,
         attachedIos: Dictionary<AttachedIoModel>,
-        portInputModes: Dictionary<PortModeName[]>
+        portInputModes: Dictionary<PortModeName[]>,
+        connectedHubIds: string[],
     ): SchemeRunBlocker[] {
-        const result: SchemeRunBlocker[] = [];
-        let necessaryModesPresent = false;
-        let attachedIosPresent = false;
         switch (widgetConfig.widgetType) {
             case WidgetType.Temperature:
-                attachedIosPresent = attachedIos[attachedIosIdFn(widgetConfig)] !== undefined;
-                necessaryModesPresent = this.temperatureWidgetConfigFactoryService.canBeUsedWithInputModes(portInputModes[attachedIosIdFn(widgetConfig)] ?? []);
-                break;
+                return this.getBlockersForTemperatureWidget(widgetConfig, attachedIos, portInputModes, connectedHubIds);
             case WidgetType.Tilt:
-                attachedIosPresent = attachedIos[attachedIosIdFn(widgetConfig)] !== undefined;
-                necessaryModesPresent = this.tiltWidgetConfigFactoryService.canBeUsedWithInputModes(portInputModes[attachedIosIdFn(widgetConfig)] ?? []);
-                break;
+                return this.getBlockersForTiltWidget(widgetConfig, attachedIos, portInputModes, connectedHubIds);
             case WidgetType.Voltage:
-                attachedIosPresent = attachedIos[attachedIosIdFn(widgetConfig)] !== undefined;
-                necessaryModesPresent = this.voltageWidgetConfigFactoryService.canBeUsedWithInputModes(portInputModes[attachedIosIdFn(widgetConfig)] ?? []);
-                break;
+                return this.getBlockersForVoltageWidget(widgetConfig, attachedIos, portInputModes, connectedHubIds);
         }
-        if (!necessaryModesPresent) {
+    }
+
+    private getBlockersForVoltageWidget(
+        widgetConfig: VoltageWidgetConfigModel,
+        attachedIos: Dictionary<AttachedIoModel>,
+        portInputModes: Dictionary<PortModeName[]>,
+        connectedHubIds: string[],
+    ): SchemeRunBlocker[] {
+        const result: SchemeRunBlocker[] = [];
+        if (!this.voltageWidgetConfigFactoryService.canBeUsedWithInputModes(portInputModes[attachedIosIdFn(widgetConfig)] ?? [])) {
             result.push(SchemeRunBlocker.SomeIosHaveNoRequiredCapabilities);
         }
-        if (!attachedIosPresent) {
+        if (attachedIos[attachedIosIdFn(widgetConfig)] === undefined) {
             result.push(SchemeRunBlocker.SomeIosAreNotConnected);
+        }
+        if (!connectedHubIds.includes(widgetConfig.hubId)) {
+            result.push(SchemeRunBlocker.SomeHubsAreNotConnected);
+        }
+        return result;
+    }
+
+    private getBlockersForTemperatureWidget(
+        widgetConfig: TemperatureWidgetConfigModel,
+        attachedIos: Dictionary<AttachedIoModel>,
+        portInputModes: Dictionary<PortModeName[]>,
+        connectedHubIds: string[],
+    ): SchemeRunBlocker[] {
+        const result: SchemeRunBlocker[] = [];
+        if (!this.temperatureWidgetConfigFactoryService.canBeUsedWithInputModes(portInputModes[attachedIosIdFn(widgetConfig)] ?? [])) {
+            result.push(SchemeRunBlocker.SomeIosHaveNoRequiredCapabilities);
+        }
+        if (attachedIos[attachedIosIdFn(widgetConfig)] === undefined) {
+            result.push(SchemeRunBlocker.SomeIosAreNotConnected);
+        }
+        if (!connectedHubIds.includes(widgetConfig.hubId)) {
+            result.push(SchemeRunBlocker.SomeHubsAreNotConnected);
+        }
+        return result;
+    }
+
+    private getBlockersForTiltWidget(
+        widgetConfig: TiltWidgetConfigModel,
+        attachedIos: Dictionary<AttachedIoModel>,
+        portInputModes: Dictionary<PortModeName[]>,
+        connectedHubIds: string[],
+    ): SchemeRunBlocker[] {
+        const result: SchemeRunBlocker[] = [];
+        if (!this.tiltWidgetConfigFactoryService.canBeUsedWithInputModes(portInputModes[attachedIosIdFn(widgetConfig)] ?? [])) {
+            result.push(SchemeRunBlocker.SomeIosHaveNoRequiredCapabilities);
+        }
+        if (attachedIos[attachedIosIdFn(widgetConfig)] === undefined) {
+            result.push(SchemeRunBlocker.SomeIosAreNotConnected);
+        }
+        if (!connectedHubIds.includes(widgetConfig.hubId)) {
+            result.push(SchemeRunBlocker.SomeHubsAreNotConnected);
         }
         return result;
     }
