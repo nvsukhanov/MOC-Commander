9bbd46afe852567b817c05e8ec884233ff626d03
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-01-21 16:59:00 +0400

    fix: add linux detection for rxpoweredup workaround

---
+++ modules/shared/misc/src/lib/is-os-linux.ts
@@ -0,0 +1,9 @@
+import { inject } from '@angular/core';
+
+import { NAVIGATOR } from './types';
+
+export function isOsLinux(
+    navigator: Navigator = inject(NAVIGATOR)
+): boolean {
+    return navigator.userAgent.toLowerCase().includes('linux');
+}
