0e4887423c355957c574a757812465c5d7acecf8
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-01-24 10:23:40 +0400

    refactor: remove old store migration code

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,9 @@ import { provideControllerProfiles } from '@app/controller-profiles';
 import { RootComponent } from '@app/root';
 import { ShowOnTouchedErrorStateMatcher } from '@app/shared-misc';
 import { provideI18n } from '@app/shared-i18n';
-import { AppStoreVersion, provideApplicationStore } from '@app/store';
+import { provideApplicationStore } from '@app/store';
 
 import { ROUTES } from './routes';
-
-// removing old ls store version prefixes.
-// TODO: remove this code after a 2023-10-15
-const prefix = [ AppStoreVersion.first, '/' ].join('');
-for (const k in window.localStorage) {
-    if (k.startsWith(prefix)) {
-        const v = window.localStorage.getItem(k);
-        if (!v) {
-            continue;
-        }
-        window.localStorage.removeItem(k);
-        window.localStorage.setItem(k.replace(prefix, ''), v);
-    }
-}
 
 bootstrapApplication(RootComponent, {
     providers: [
