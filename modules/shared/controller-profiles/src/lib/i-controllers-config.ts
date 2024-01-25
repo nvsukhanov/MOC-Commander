fddb7d082e2d3f559023ac2bfa01f440d4b15e47
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-01-25 17:07:31 +0400

    fix: process gamepad input with each animation frame (fixes high latency)

--- modules/shared/controller-profiles/src/lib/i-controllers-config.ts
+++ modules/shared/controller-profiles/src/lib/i-controllers-config.ts
@@ -2,7 +2,6 @@ import { InjectionToken } from '@angular/core';
 
 export interface IControllersConfig {
     readonly gamepad: {
-        readonly inputReadInterval: number;
         readonly defaultAxisActiveZoneStart: number;
         readonly defaultButtonActiveZoneStart: number;
         readonly defaultActivationThreshold: number;
@@ -15,7 +14,6 @@ export interface IControllersConfig {
 export const CONTROLLERS_CONFIG = new InjectionToken<IControllersConfig>('CONTROLLERS_CONFIG', {
     factory: (): IControllersConfig => ({
         gamepad: {
-            inputReadInterval: 1000 / 30,
             defaultAxisActiveZoneStart: 0.1,
             defaultButtonActiveZoneStart: 0.01,
             defaultActivationThreshold: 0.5
