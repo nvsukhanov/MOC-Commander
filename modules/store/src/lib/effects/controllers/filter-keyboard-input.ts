1158752c5ba3e6dc985e8c0c24d654ebad14cd5a
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-03-04 18:49:25 +0400

    fix: correct capturing of alphanumeric kb input

--- modules/store/src/lib/effects/controllers/filter-keyboard-input.ts
+++ modules/store/src/lib/effects/controllers/filter-keyboard-input.ts
@@ -5,7 +5,6 @@ export function filterKeyboardInput(
 ): OperatorFunction<Event, KeyboardEvent> {
     return (source) => source.pipe(
         filter((event): event is KeyboardEvent => !!(event as KeyboardEvent).key),
-        filter((event) => !captureNonAlphaNumerics || event.key.length === 1),
-        filter((event) => captureNonAlphaNumerics || /^[\p{L}\p{N}]+$/u.test(event.key))
+        filter((event) => captureNonAlphaNumerics || /^[\p{L}\p{N}]$/u.test(event.key))
     );
 }
