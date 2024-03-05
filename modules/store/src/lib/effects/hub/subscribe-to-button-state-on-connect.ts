72ed66e66166a80a8656ddc4ffa8e48db160b758
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-03-05 18:46:52 +0400

    fix: do not unsubscribe from hub properties after hub disconnected
    fixes error in console after hub is disconnected

--- modules/store/src/lib/effects/hub/subscribe-to-button-state-on-connect.ts
+++ modules/store/src/lib/effects/hub/subscribe-to-button-state-on-connect.ts
@@ -1,5 +1,5 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap, takeUntil } from 'rxjs';
+import { map, mergeMap } from 'rxjs';
 import { inject } from '@angular/core';
 
 import { HUBS_ACTIONS, HUB_RUNTIME_DATA_ACTIONS } from '../../actions';
@@ -7,13 +7,13 @@ import { HubStorageService } from '../../hub-storage.service';
 
 export const SUBSCRIBE_TO_BUTTON_STATE_ON_CONNECT = createEffect((
     actions$: Actions = inject(Actions),
-    hubStorage: HubStorageService = inject(HubStorageService),
+    hubStorage: HubStorageService = inject(HubStorageService)
 ) => {
     return actions$.pipe(
         ofType(HUBS_ACTIONS.connected),
         mergeMap((a) => hubStorage.get(a.hubId).properties.buttonState.pipe(
-            takeUntil(hubStorage.get(a.hubId).disconnected),
             map((isButtonPressed) => HUB_RUNTIME_DATA_ACTIONS.buttonStateReceived({ hubId: a.hubId, isButtonPressed }))
         ))
     );
 }, { functional: true });
+
