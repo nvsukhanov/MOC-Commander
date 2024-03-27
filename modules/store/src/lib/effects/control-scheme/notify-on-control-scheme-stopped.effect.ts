d5f51aed7173a209e4500d744c86348b414b6e93
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-03-27 18:50:23 +0400

    fix(a11y): address a11y issues
    + descriptive text for nav links in cs view
    + descriptive text for reorder widgets buttons
    + fix screenSizeObserver reports
    + add notifications on control scheme start/stop

---
+++ modules/store/src/lib/effects/control-scheme/notify-on-control-scheme-stopped.effect.ts
@@ -0,0 +1,16 @@
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { inject } from '@angular/core';
+import { map } from 'rxjs';
+
+import { CONTROL_SCHEME_ACTIONS, SHOW_NOTIFICATION_ACTIONS } from '../../actions';
+
+export const NOTIFY_ON_CONTROL_SCHEME_STOPPED_EFFECT = createEffect((
+    actions: Actions = inject(Actions),
+) => {
+    return actions.pipe(
+        ofType(CONTROL_SCHEME_ACTIONS.schemeStopped),
+        map(() => {
+            return SHOW_NOTIFICATION_ACTIONS.info({ l10nKey: 'controlScheme.stopNotification'});
+        })
+    );
+}, { functional: true });
